2ba7ddca4b504aa02def909c7c41f1a6f2943011
Adam Matthiesen <amatthiesen@outlook.com>
2026-07-21 01:18:20 -0700

    Add mixins for inventory screens and item picker menu

---
+++ neoforge/src/main/java/dev/matthiesen/matthiesen_lib/neoforge/mixins/AbstractContainerScreenNeoForgeMixin.java
@@ -0,0 +1,8 @@
+package dev.matthiesen.matthiesen_lib.neoforge.mixins;
+
+import net.minecraft.client.gui.screens.inventory.AbstractContainerScreen;
+import org.spongepowered.asm.mixin.Mixin;
+
+@Mixin(AbstractContainerScreen.class)
+public abstract class AbstractContainerScreenNeoForgeMixin {
+}
