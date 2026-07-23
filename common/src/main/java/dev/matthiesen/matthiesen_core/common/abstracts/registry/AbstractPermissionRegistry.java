20026328e438f628281298dfe3cb5394804da95b
Adam Matthiesen <amatthiesen@outlook.com>
2026-07-22 22:09:49 -0700

    Add abstract permission registry class for managing permissions

---
+++ common/src/main/java/dev/matthiesen/matthiesen_core/common/abstracts/registry/AbstractPermissionRegistry.java
@@ -0,0 +1,26 @@
+package dev.matthiesen.matthiesen_core.common.abstracts.registry;
+
+import dev.matthiesen.matthiesen_core.common.MatthiesenCoreCommon;
+import dev.matthiesen.matthiesen_core.common.api.permissions.Permission;
+
+/**
+ * An abstract implementation of a permission registry that provides basic functionality for registering permissions with the MatthiesenCoreCommon permissions manager.
+ * Subclasses should extend this class and implement the necessary methods to define and register their own permissions.
+ */
+@SuppressWarnings("unused")
+public abstract class AbstractPermissionRegistry {
+    /**
+     * Constructs a new AbstractPermissionRegistry. This constructor is protected to prevent direct instantiation of the abstract class. Subclasses
+     * should call this constructor to initialize the registry.
+     */
+    protected AbstractPermissionRegistry() {}
+
+    /**
+     * Registers a permission with the MatthiesenCoreCommon permissions manager.
+     * @param permission the permission to register
+     */
+    protected final void register(Permission permission) {
+        MatthiesenCoreCommon.INSTANCE.getPermissionsManager().registerPermission(permission);
+    }
+}
+
