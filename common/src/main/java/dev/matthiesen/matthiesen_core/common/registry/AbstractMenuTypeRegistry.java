a6ce36b27f5263454ce4b49e722cd0b7c0481d50
Adam Matthiesen <amatthiesen@outlook.com>
2026-07-31 16:08:25 -0700

    Add overloaded register methods for MenuType with feature flag support

--- common/src/main/java/dev/matthiesen/matthiesen_core/common/registry/AbstractMenuTypeRegistry.java
+++ common/src/main/java/dev/matthiesen/matthiesen_core/common/registry/AbstractMenuTypeRegistry.java
@@ -2,7 +2,11 @@ package dev.matthiesen.matthiesen_core.common.registry;
 
 import dev.matthiesen.matthiesen_core.common.api.platform.registry.SupportedRegistries;
 import dev.matthiesen.matthiesen_core.common.core.registry.RegistryBuilder;
+import net.minecraft.world.flag.FeatureFlagSet;
+import net.minecraft.world.inventory.AbstractContainerMenu;
 import net.minecraft.world.inventory.MenuType;
+
+import java.util.function.Supplier;
 
 /**
  * Convenience base class for registries that register {@link MenuType} instances.
@@ -28,6 +32,29 @@ public abstract class AbstractMenuTypeRegistry extends AbstractRegistry<MenuType
      */
     protected AbstractMenuTypeRegistry(RegistryBuilder registryBuilder) {
         super(registryBuilder, SupportedRegistries.MENU_TYPE);
+    }
+
+    /**
+     * Registers a menu type with the given name and supplier.
+     * @param name the name of the menu type
+     * @param supplier the supplier for the menu type
+     * @return a supplier for the registered menu type
+     * @param <T> the type of the menu
+     */
+    public <T extends AbstractContainerMenu> Supplier<MenuType<T>> register(String name, MenuType.MenuSupplier<T> supplier) {
+        return register(name, () -> new MenuType<>(supplier, FeatureFlagSet.of()));
+    }
+
+    /**
+     * Registers a menu type with the given name, supplier, and feature flag set.
+     * @param name the name of the menu type
+     * @param supplier the supplier for the menu type
+     * @param featureFlagSet the feature flag set for the menu type
+     * @return a supplier for the registered menu type
+     * @param <T> the type of the menu
+     */
+    public <T extends AbstractContainerMenu> Supplier<MenuType<T>> register(String name, MenuType.MenuSupplier<T> supplier, FeatureFlagSet featureFlagSet) {
+        return register(name, () -> new MenuType<>(supplier, featureFlagSet));
     }
 }
 
